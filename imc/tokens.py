16a8bfbe4c1b0b7161038be608c65b03bf7cb3da
Andrew Lahiff <andrew.lahiff@gmail.com>
2019-08-12 10:55:55 +0000

    Name of delete_token & set_token methods were not correct

--- imc/tokens.py
+++ imc/tokens.py
@@ -46,10 +46,10 @@ def get_token(cloud, db, config_file):
         (token, expiry, creation, msg) = get_new_token(username, password, client_id, client_secret, refresh_token, scope, url)
 
         # Delete existing token from DB
-        db_delete_token(cloud)
+        db.delete_token(cloud)
 
         # Save token to DB
-        db_set_token(cloud, token, expiry, creation)
+        db.set_token(cloud, token, expiry, creation)
     else:
         logger.info('Using token from DB for cloud %s', cloud)
 
