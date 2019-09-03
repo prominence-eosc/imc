437479e4c46a321ec851b3ab7e6feea303aa1c2f
Andrew Lahiff <andrew.lahiff@gmail.com>
2019-09-03 13:15:08 +0000

    Nova client context manager no longer supported

--- imc/cloud_quotas.py
+++ imc/cloud_quotas.py
@@ -39,8 +39,8 @@ def get_quotas_openstack(cloud, credentials):
             'user_domain_name':credentials['user_domain_name']}
 
     try:
-        with client.Client(2, credentials['username'], insecure=True, timeout=30, **auth) as nova:
-            quotas = nova.quotas.get(credentials['tenant_id'], detail=True)
+        nova = client.Client(2, credentials['username'], insecure=True, timeout=30, **auth)
+        quotas = nova.quotas.get(credentials['tenant_id'], detail=True)
     except Exception as ex:
         logger.critical('Unable to get quotas from cloud %s due to "%s"', cloud, ex)
         return (None, None, None)
