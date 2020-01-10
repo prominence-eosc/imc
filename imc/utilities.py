714b154850d3035963162af0fdefc2f935081046
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-01-10 13:52:34 +0000

    Add functions for extracting RADL, requirements & preferences from JSON description

--- imc/utilities.py
+++ imc/utilities.py
@@ -1,5 +1,6 @@
 """Miscellaneous functions"""
 
+import base64
 import glob
 import json
 import logging
@@ -332,3 +333,46 @@ def valid_uuid(uuid):
     regex = re.compile('^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z', re.I)
     match = regex.match(uuid)
     return bool(match)
+
+def get_radl(description):
+    """
+    Extract & decode RADL from input JSON description
+    """
+    if 'radl' in description:
+        try:
+            radl = base64.b64decode(description['radl'])
+        except Exception as err:
+            logger.warning('Invalid RADL provided: cannot be decoded')
+            return None
+
+        return radl
+    return None
+
+def get_reqs_and_prefs(description):
+    """
+    Extract the requirements & preferences from the input JSON description
+    """
+    if 'preferences' in description:
+        preferences_new = {}
+        # Generate list of weighted regions if necessary
+        if 'regions' in description['preferences']:
+            preferences_new['regions'] = {}
+            for i in range(0, len(description['preferences']['regions'])):
+                preferences_new['regions'][description['preferences']['regions'][i]] = len(description['preferences']['regions']) - i
+        # Generate list of weighted sites if necessary
+        if 'sites' in description['preferences']:
+            preferences_new['sites'] = {}
+            for i in range(0, len(description['preferences']['sites'])):
+                preferences_new['sites'][description['preferences']['sites'][i]] = len(description['preferences']['sites']) - i
+        description['preferences'] = preferences_new
+    else:
+        preferences = {}
+
+    if 'requirements' in description:
+        requirements = description['requirements']
+        preferences = description['preferences']
+    else:
+        requirements = {}
+    
+    return (requirements, preferences)
+
