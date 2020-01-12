b8d9f6530cdcc03fb5a32ff035475d372f7ae034
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-01-12 08:32:28 +0000

    Add psutil module

---
+++ imc/destroyer.py
@@ -0,0 +1,41 @@
+""" Worker pool """
+
+from __future__ import print_function
+import configparser
+import logging
+import re
+import os
+import sys
+import time
+
+import database
+import imc
+import imclient
+import opaclient
+import tokens
+import utilities
+import logger as custom_logger
+
+# Configuration
+CONFIG = configparser.ConfigParser()
+if 'PROMINENCE_IMC_CONFIG_DIR' in os.environ:
+    CONFIG.read('%s/imc.ini' % os.environ['PROMINENCE_IMC_CONFIG_DIR'])
+else:
+    print('ERROR: Environment variable PROMINENCE_IMC_CONFIG_DIR has not been defined')
+    exit(1)
+
+if __name__ == "__main__":
+    infra_id = sys.argv[1]
+    logging.basicConfig(filename=CONFIG.get('logs', 'filename').replace('.log', '-destroyer-%s.log' % infra_id),
+                        level=logging.INFO,
+                        format='%(asctime)s %(message)s')
+
+    logger.info('Starting deletion of infrastructure')
+
+    db = database.get_db()
+    if db.connect():
+        imc.delete(infra_id)
+    db.close()
+
+    logger.info('Completed deleting infrastructure')
+
