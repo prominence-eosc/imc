e8573e539279078d0455adb1e5fde13b28a12b76
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-03-13 10:26:55 +0000

    Add template for batch_deploy

---
+++ imc/batch_deploy.py
@@ -0,0 +1,27 @@
+"""Run pilot job on the specified batch system, with extensive error handling"""
+
+from __future__ import print_function
+import os
+import sys
+from string import Template
+import time
+import random
+import logging
+import configparser
+
+from imc import database
+from imc import destroy
+from imc import opaclient
+from imc import tokens
+from imc import utilities
+
+# Configuration
+CONFIG = utilities.get_config()
+
+# Logging
+logger = logging.getLogger(__name__)
+
+def batch_deploy(radl, resource_name, time_begin, unique_id, identity, db, num_nodes=1):
+    """
+    Submit a startd job to the specified batch system
+    """
