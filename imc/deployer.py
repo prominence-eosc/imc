16606124df3ebdfbfe31b7bda5dd6cac9ba1779a
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-05-31 06:41:29 +0000

    Support batch system submission

--- imc/deployer.py
+++ imc/deployer.py
@@ -13,35 +13,37 @@ from imc import utilities
 # Configuration
 CONFIG = config.get_config()
 
-def deployer(infra_id):
+# Logging
+logger = logging.getLogger('imc.deployer')
+
+def deployer(infra_id, batch_client):
     """
     Deploy infrastructure
     """
-    logging.basicConfig(filename=CONFIG.get('logs', 'filename').replace('.log', '-deploy-%s.log' % infra_id),
-                        level=logging.INFO,
-                        format='%(asctime)s %(message)s')
-
-    logging.info('Starting deployment of infrastructure %s', infra_id)
+    logger.info('Starting deployment of infrastructure %s', infra_id)
 
     # Random sleep
     time.sleep(random.randint(0, 4))
 
     db = database.get_db()
     if db.connect():
-        logging.info('Connected to DB, about to deploy infrastructure for job')
+        logger.info('Connected to DB, about to deploy infrastructure for job')
 
         # Deploy infrastructure
-        success = provisioner.deploy_job(db, infra_id)
+        try:
+            success = provisioner.deploy_job(db, batch_client, infra_id)
+        except Exception as exc:
+            logger.info('Got exception: %s', exc)
 
         if success is None:
-            logging.info('Setting status to unable - permanent failure')
+            logger.info('Setting status to unable due to a permanent failure')
             db.deployment_update_status_with_retries(infra_id, 'unable')
         elif not success:
-            logging.info('Setting status to waiting - temporary failure')
+            logger.info('Setting status to waiting due to a temporary failure')
             db.deployment_update_status_with_retries(infra_id, 'waiting')
         db.close()
 
     if not success:
-        logging.critical('Unable to deploy infrastructure on any cloud')
+        logger.critical('Unable to deploy infrastructure on any cloud')
 
-    logging.info('Completed deploying infrastructure')
+    logger.info('Completed deploying infrastructure')
