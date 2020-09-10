78f7c9b956c624db08bebcd18ffa176ae7950db6
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-09-10 13:44:04 +0000

    The check wasn't strictly necessary since 'waiting' would not overwrite a del* state

--- imc/deployer.py
+++ imc/deployer.py
@@ -40,10 +40,8 @@ def deployer(infra_id, batch_client):
             logger.info('Setting status to unable due to a permanent failure')
             db.deployment_update_status_with_retries(infra_id, 'unable')
         elif not success:
-            (_, status, _, _, _) = db.deployment_get_im_infra_id(infra_id)
-            if 'del' not in status:
-                logger.info('Setting status to waiting due to a temporary failure')
-                db.deployment_update_status_with_retries(infra_id, 'waiting')
+            logger.info('Setting status to waiting due to a temporary failure')
+            db.deployment_update_status_with_retries(infra_id, 'waiting')
         db.close()
 
     if not success:
