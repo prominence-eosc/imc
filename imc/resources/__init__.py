af9dce758e27223113e72faaf37744690f3f106b
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-05-30 12:52:49 +0000

    Add AWS

--- imc/resources/__init__.py
+++ imc/resources/__init__.py
@@ -1,11 +1,13 @@
 from .openstack import OpenStack
+from .aws import AWS
 
 def Resource(resource_type, *kwargs):
     """
     Factory method for different resources
     """
     resources = {
-        "openstack": OpenStack
+        "openstack": OpenStack,
+        "aws": AWS
     }
 
     return resources[resource_type](*kwargs)
